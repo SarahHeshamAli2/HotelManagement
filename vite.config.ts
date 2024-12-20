94fb74c7a443c4846cf00f7a4d5abf0548e1b6ac
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-21 01:13:25 +0200

    fail build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split dependencies into separate chunks
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit if needed
+  }
 })
