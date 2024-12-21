bd447e7bc59d1dcc35ef4599457d31e84ab09077
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-21 16:31:27 +0200

    authContext duplicated

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify';
-import AuthContextProvider from './Context/AuthContext.tsx';
-
+import AuthContextProvider from './Context/AuthContext';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <AuthContextProvider>
