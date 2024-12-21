6878f8be3859ee9672365d84a34d0085cb93bb3a
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-21 16:36:38 +0200

    d

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify';
-import AuthContextProvider from './Context/AuthContext';
+import AuthContextProvider from './Context/AuthContext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <AuthContextProvider>
