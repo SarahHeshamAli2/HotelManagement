85a25605b5575d7666329dce3227b5995c986ac7
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-21 16:49:22 +0200

    authCont

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ToastContainer } from 'react-toastify';
 import AuthContextProvider from './Context/AuthContext.tsx'
+import { ToastContainer } from 'react-toastify';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <AuthContextProvider>
