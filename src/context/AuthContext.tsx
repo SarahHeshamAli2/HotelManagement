7d4ec626509d24a33e9d5ba72129ccbfcfff1cd2
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-21 16:22:19 +0200

    deploy

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { jwtDecode } from "jwt-decode";
 import { createContext, useEffect, useState } from "react";
-import { AUTH_URLS, axiosInstance } from "../services/urls";
 
 interface AuthData {
   saveLoginData: () => void;
@@ -13,40 +12,16 @@ export const AuthContext = createContext<AuthData | any>(null);
 
 export default function AuthContextProvider(props: any) {
   const [loginData, setLoginData] = useState(null);
-  const [userName, setUserName] = useState(null);
-  const [profileImage, setProfileImage] = useState(null);
   const saveLoginData = () => {
     const encodedToken: any = localStorage.getItem("token");
     const decodedToken: any = jwtDecode(encodedToken);
-    
-    if(loginData) {
-      getCurrentUser()
-  }
     setLoginData(decodedToken);
   };
-
-
-  const getCurrentUser = ()=>{
-    axiosInstance.get(AUTH_URLS.get_current_user(loginData?._id)).then((response)=>{
-      console.log(response?.data?.data.user.profileImage);
-      setUserName(response?.data?.data?.user?.userName)
-      setProfileImage(response?.data?.data.user.profileImage)
-      
-    }).catch((err)=>{
-      console.log(err);
-      
-    })
-
-  }
   useEffect(() => {
     if (localStorage.getItem("token")) {
       saveLoginData();
-
-    
-      
- 
     }
-  }, [loginData?.role]);
+  }, []);
 
   const logout = () => {
     localStorage.removeItem("token");
@@ -54,7 +29,7 @@ export default function AuthContextProvider(props: any) {
   };
 
   return (
-    <AuthContext.Provider value={{ loginData, saveLoginData, logout,userName,profileImage }}>
+    <AuthContext.Provider value={{ loginData, saveLoginData, logout }}>
       {props.children}
     </AuthContext.Provider>
   );
