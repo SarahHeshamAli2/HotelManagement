1363ab9720fefdaa6d966f9f7986984c4402e5ed
menna alaa <mennaelgindy98@gmail.com>
2024-12-24 14:31:38 +0200

    date-picker

--- src/modules/Users-Portal/Component/UsersShared/CalendarBooking/CalendarBooking.tsx
+++ src/modules/Users-Portal/Component/UsersShared/CalendarBooking/CalendarBooking.tsx
@@ -1,40 +1,33 @@
-import { Add, CalendarMonth, Remove } from "@mui/icons-material";
-import {
-  Box,
-  Button,
-  IconButton,
-  TextField,
-  Typography,
-  FormHelperText,
-} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
-import { AxiosError } from "axios";
-import { useNavigate } from "react-router-dom";
+
 import img from "../../../../../assets/images/banner.png";
-import { axiosInstance, getRoomDetails } from "../../../../../services/urls";
-import { toast } from "react-toastify";
-import dayjs from "dayjs";
 import DatePicker from "../CalendarBooking/DatePicker/DatePicker";
+import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
+import { Add, Remove } from "@mui/icons-material";
+import { axiosInstance, getRoomDetails } from "../../../../../services/urls";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 export default function CalendarBooking() {
-  const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [dateRange, setDateRange] = useState<{ startDate: Date | null; endDate: Date | null }>({
+  const [dateRange, setDateRange] = useState<{
+    startDate: Date | null;
+    endDate: Date | null;
+  }>({
     startDate: null,
     endDate: null,
   });
-  const [count, setCount] = useState(1);
   const [error, setError] = useState<string>("");
 
-  const handleButtonClick = (e: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(e.currentTarget);
-  };
+  const navigate = useNavigate();
+  const [count, setCount] = useState(1);
 
   const handlePopoverClose = () => {
     setAnchorEl(null);
   };
-
   const handleIncrease = () => {
     setCount(count + 1);
   };
@@ -44,7 +37,6 @@ export default function CalendarBooking() {
       setCount(count - 1);
     }
   };
-
   const getRooms = async () => {
     if (!dateRange.startDate || !dateRange.endDate) {
       setError("Please pick a start and end date.");
@@ -102,105 +94,75 @@ export default function CalendarBooking() {
             We provide what you need to enjoy your holiday with family.
             <br /> Time to make another memorable moment.
           </Typography>
-          <Box>
-            <Typography
-              variant="h3"
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: "600",
+              fontSize: { xs: "1.25rem", sm: "1.5rem" },
+              marginBottom: "0.1rem",
+              color: "#152C5B",
+              lineHeight: "1.875rem",
+              mb: "1rem",
+            }}
+          >
+            Start Booking
+          </Typography>
+          <DatePicker
+            dateRange={dateRange}
+            setDateRange={setDateRange}
+            error={error}
+            setError={setError}
+            anchorEl={anchorEl}
+            setAnchorEl={setAnchorEl}
+            onClose={handlePopoverClose}
+          />
+          <Box sx={{ mt: "1.5rem", display: "flex", alignItems: "center" }}>
+            <IconButton
+              onClick={handleDecrease}
               sx={{
-                fontWeight: "600",
-                fontSize: { xs: "1.25rem", sm: "1.5rem" },
-                marginBottom: "0.1rem",
-                color: "#152C5B",
-                lineHeight: "1.875rem",
-                mb: "1rem",
-              }}
-            >
-              Start Booking
-            </Typography>
-            <Button
-              sx={{
-                padding: "15px 20px",
-                borderRadius: "12px",
-                mr: "10px",
-              }}
-              onClick={handleButtonClick}
-              variant="contained"
-              color="primary"
-            >
-              <CalendarMonth />
-            </Button>
-            <DatePicker
-              dateRange={dateRange}
-              setDateRange={setDateRange}
-              error={error}
-              setError={setError}
-              anchorEl={anchorEl}
-              onClose={handlePopoverClose}
-            />
-            <TextField
-              onClick={handleButtonClick}
-              label="Pick a Date"
-              value={
-                dateRange.startDate && dateRange.endDate
-                  ? `${dayjs(dateRange.startDate).format("YYYY-MM-DD")} - ${dayjs(
-                      dateRange.endDate
-                    ).format("YYYY-MM-DD")}`
-                  : "Pick a Start & End Date"
-              }
-              error={Boolean(error)}
-            />
-            {error && (
-              <FormHelperText error sx={{ ml: "5rem" }}>
-                {error}
-              </FormHelperText>
-            )}
-            <Box sx={{ mt: "1.5rem", display: "flex", alignItems: "center" }}>
-              <IconButton
-                onClick={handleDecrease}
-                sx={{
-                  width: "3.5rem",
+                width: "3.5rem",
+                backgroundColor: "#E74C3C",
+                borderRadius: "4px 0px 0px 0px",
+                "&:hover": {
                   backgroundColor: "#E74C3C",
-                  borderRadius: "4px 0px 0px 0px",
-                  "&:hover": {
-                    backgroundColor: "#E74C3C",
-                  },
-                  mr: "1rem",
-                }}
-              >
-                <Remove sx={{ color: "#fff" }} />
-              </IconButton>
-              <TextField
-                sx={{ color: "#152C5B" }}
-                label="Capacity"
-                value={`${count} person`}
-              />
-              <IconButton
-                onClick={handleIncrease}
-                sx={{
-                  width: "3.5rem",
-                  backgroundColor: "#1ABC9C",
-                  borderRadius: "0px 4px 0px 0px",
-                  "&:hover": {
-                    backgroundColor: "#1ABC9C",
-                  },
-                  ml: "1rem",
-                }}
-              >
-                <Add sx={{ color: "white" }} />
-              </IconButton>
-            </Box>
-            <Button
-              sx={{
-                mt: "2rem",
-                backgroundColor: "#3252DF",
-                color: "white",
-                paddingBlock: "1rem",
-                paddingInline: "5rem",
+                },
+                mr: "1rem",
               }}
-              onClick={getRooms}
             >
-              Explore
-            </Button>
+              <Remove sx={{ color: "#fff" }} />
+            </IconButton>
+            <TextField
+              sx={{ color: "#152C5B" }}
+              label="Capacity"
+              value={`${count} person`}
+            />
+            <IconButton
+              onClick={handleIncrease}
+              sx={{
+                width: "3.5rem",
+                backgroundColor: "#1ABC9C",
+                borderRadius: "0px 4px 0px 0px",
+                "&:hover": {
+                  backgroundColor: "#1ABC9C",
+                },
+                ml: "1rem",
+              }}
+            >
+              <Add sx={{ color: "white" }} />
+            </IconButton>
           </Box>
+          <Button
+            sx={{
+              mt: "2rem",
+              backgroundColor: "#3252DF",
+              color: "white",
+              paddingBlock: "1rem",
+              paddingInline: "5rem",
+            }}
+            onClick={getRooms}
+          >
+            Explore
+          </Button>
         </Grid>
         <Grid item xs={12} sm={4}>
           <Box
