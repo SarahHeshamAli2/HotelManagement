76d97b55e9140c511d2fbe8bdc3efea34d851003
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-24 21:13:36 +0200

    remove protected route

---
+++ src/modules/Shared/Components/ProtectedRoute/UserProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useContext } from 'react'
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../../../../Context/Context';
+
+interface ProtectedRouteProps {
+    children: ReactNode;
+  }
+
+export default function UserProtectedRoute({ children }: ProtectedRouteProps) {
+  const { loginData } = useContext(AuthContext);
+
+
+  if (localStorage.getItem('token') && loginData?.role === 'user') {
+    return <>{children}</>;
+  } else {
+    return <Navigate to="/login" />;
+  }
+
+}
