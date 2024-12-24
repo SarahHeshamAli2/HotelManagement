30a339fae659cece491ae1fbff196b18b8f4dd90
Mariam Saeed <mariamsaeed750@gmail.com>
2024-12-24 02:03:56 +0200

    MS/change-emotion-color

--- src/modules/Users-Portal/Component/UsersShared/Ads/Ads.tsx
+++ src/modules/Users-Portal/Component/UsersShared/Ads/Ads.tsx
@@ -1,6 +1,20 @@
+import { Box, Typography } from "@mui/material";
 
 export default function Ads() {
   return (
-    <div>Ads</div>
+    <Box>
+      <Typography
+				variant='body1'
+				component='h2'
+				sx={{
+					fontWeight: '500',
+					fontSize: '1.5rem',
+					marginBottom: '20px',
+					color: '#152C5B',
+				}}
+			>
+				Ads
+			</Typography>
+    </Box>
   )
 }
