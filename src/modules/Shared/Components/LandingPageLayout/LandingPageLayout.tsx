9d1a5b75a98f1687bb71f4c64c7d1eb2f1afc596
menna alaa <mennaelgindy98@gmail.com>
2024-12-26 14:31:04 +0200

    booking-routing

--- src/modules/Shared/Components/LandingPageLayout/LandingPageLayout.tsx
+++ src/modules/Shared/Components/LandingPageLayout/LandingPageLayout.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import LandingPageNavbar from "../LandingPageNavbar/LandingPageNavbar";
 import BackToTopButton from "../../BackToTopButton/BackToTop";
 export default function LandingPageLayout() {
-  return (
+  let {pathname} = useLocation();
+  const paymentPathRegex = /^\/booking\/[^/]+\/(user-info|Payment-info|booking-success)$/;
+  console.log(pathname)
+  return (    
     <>
-      <LandingPageNavbar />
+    
+    {!paymentPathRegex.test(pathname) && <LandingPageNavbar />}
       <Outlet />
-      <Footer />
+      {!paymentPathRegex.test(pathname) && <Footer />}
       <BackToTopButton/>
     </>
   );
